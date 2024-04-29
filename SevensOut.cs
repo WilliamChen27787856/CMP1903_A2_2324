0fc440caae1ac056d664a2233370c6e0f2dfa732
cv747 <144601471+cv747@users.noreply.github.com>
2024-04-29 10:51:35 +0100

    added more XML comments to games

--- SevensOut.cs
+++ SevensOut.cs
@@ -5,32 +5,50 @@ namespace CMP1903_A2_2324 {
 
   public sealed class SevensOut : Game {
 
-    private static readonly string GAME_NAME = "Sevens Out";
-    private static readonly int DICE_COUNT = 2;
+    /// <value>
+    /// A constant value for the game's identifier.
+    /// </value>
+    private const string GAME_NAME = "Sevens Out";
 
+    /// <value>
+    /// A constant value for the number of dice that are inside of the game.
+    /// </value>
+    private const int DICE_COUNT = 2;
+
+    /// <summary>
+    /// The constructor for the class, it is used to instantiate the default values for the game
+    /// </summary>
+    /// <param name="againstComputer">
+    /// If the game should be against a computer or not.
+    /// </param>
     public SevensOut(bool againstComputer) : base(GAME_NAME, DICE_COUNT, 6, againstComputer) {
     }
 
+    /// <summary>
+    /// This method is an override of an abstract method from the <c>Game</c> class.
+    /// This method implements the game requirements for the Sevens Out game rules.
+    /// </summary>
+    /// <returns>
+    /// A boolean for if the game should continue or not, true = continue, false = exit.
+    /// </returns>
     public override bool NextMove() {
       int[] rolledDice = this.RollDice();
       int total = rolledDice.Sum();
 
-      int dieOne = rolledDice[0];
-      int dieTwo = rolledDice[1];
-
-      this.PrintRolledDice();
+      this.PrintRolledDice(); // Print out what the player rolled.
 
       if (total == 7) {
         Game.ScreenPrint($"{this.GetPlayerName()} has lost...");
         return false; // Game over...
       }
 
-      if (dieOne == dieTwo) {
-        total *= 2;
+      if (rolledDice[0] == rolledDice[1]) {
+        Game.ScreenPrint($"{this.GetPlayerName()} rolled the same numbers, score 2x!");
+        total *= 2; // Score doubles if both die landed on the same side.
       }
 
-      this.AddScorePlayer(total);
-      this.SwitchPlayer();
+      this.AddScorePlayer(total); // Add the score to the player.
+      this.SwitchPlayer(); // Switch to the next player.
       return true; // Continue playing.
     }
 
--- ThreeOrMore.cs
+++ ThreeOrMore.cs
@@ -5,61 +5,100 @@ using System.Linq;
 namespace CMP1903_A2_2324 {
 
   public sealed class ThreeOrMore : Game {
-    private static readonly string GAME_NAME = "Three Or More";
-    private static readonly int DICE_COUNT = 5;
 
+    /// <value>
+    /// A constant value for the game's identifier.
+    /// </value>
+    private const string GAME_NAME = "Three Or More";
+
+    /// <value>
+    /// A constant value for the number of dice that are inside of the game.
+    /// </value>
+    private const int DICE_COUNT = 5;
+
+    /// <summary>
+    /// The constructor for the class, it is used to instantiate the default values for the game
+    /// </summary>
+    /// <param name="againstComputer">
+    /// If the game should be against a computer or not.
+    /// </param>
     public ThreeOrMore(bool againstComputer) : base(GAME_NAME, DICE_COUNT, 6, againstComputer) {
     }
 
+    /// <summary>
+    /// This method is an override of an abstract method from the <c>Game</c> class.
+    /// This method implements the game requirements for the Three Or More game rules.
+    /// </summary>
+    /// <returns>
+    /// A boolean for if the game should continue or not, true = continue, false = exit.
+    /// </returns>
     public override bool NextMove() {
-      this.RollDice();
-      this.PrintRolledDice();
+      int[] diceRolled = this.RollDice();
+      this.PrintRolledDice(); // Print out what the player rolled.
 
-      this.CheckTwoOfAKind();
+      this.CheckTwoOfAKind(diceRolled); // Check if the player rolled a two of a kind.
 
-      int total = ThreeOrMore.GetScoreFromValues(this.DieValues);
-      this.AddScorePlayer(total);
+      int total = ThreeOrMore.GetScoreFromValues(this.DieValues); // Find the player's score.
+      this.AddScorePlayer(total); // Add the score to the player.
 
-      if (this.GetScorePlayer() >= 20) {
+      if (this.GetScorePlayer() >= 20) { // If their total score is over 20, game over.
         return false;
       }
 
-      this.SwitchPlayer();
+      this.SwitchPlayer(); // Switch to the next player.
       return true; // Continue playing.
     }
 
-    private void CheckTwoOfAKind() {
-      int twoOfAKind = TwoOfAKind();
+    /// <summary>
+    /// This method is used to simplify the main code in the <c>NextMove</c> method, it contains
+    /// the logic for if the player should be allowed to re-roll their dice.
+    /// </summary>
+    private void CheckTwoOfAKind(int[] diceRolled) {
+      int twoOfAKind = TwoOfAKind(diceRolled); // Check if the player scored a two of a kind.
 
-      if (twoOfAKind == -1) {
-        return;
+      if (twoOfAKind == -1) { // Player did not have any two of a kinds.
+        return; // Return back to main game logic.
       }
 
-      bool computerPlaying = this.IsPlayerComputer();
-      bool doLockDie = computerPlaying;
+      bool computerPlaying = this.IsPlayerComputer(); // Check if current player is the computer.
+      bool doLockDie = computerPlaying; // Always perform double locking if computer playing.
 
       Game.ScreenPrint($"{this.GetPlayerName()} has got a double.");
-      if (!computerPlaying) {
+      if (!computerPlaying) { // Ask non-computer players if they would like to re-roll.
         Game.ScreenPrint("Would you like to re-roll the others?");
-        string reRoll = Game.Choice(new string[] { "All", "Remaining", "No" });
+        string reRoll = Game.Choice(new string[] { "All", "Remaining", "No" }); // Get user choice.
 
         if (reRoll == "No") {
-          return;
+          return; // No need to perform re-roll logic, exit early.
         }
 
-        doLockDie = reRoll == "Remaining";
+        doLockDie = reRoll == "Remaining"; // Lock doubles if only rolling remaining, otherwise
+                                           // don't lock dice.
       }
 
       if (doLockDie) {
-        this.LockTwoDice(twoOfAKind);
+        this.LockDice(twoOfAKind); // Lock all dice that have the same value as the double.
       }
 
-      this.RollDice();
-      this.PrintRolledDice();
+      this.RollDice(); // Re-roll the dice.
+      this.PrintRolledDice(); // Print the new dice values.
     }
 
-    private int TwoOfAKind() {
-      foreach (KeyValuePair<int, int> kv in ThreeOrMore.GetFrequencies(this.DieValues)) {
+    /// <summary>
+    /// A method that is used to lock all dice with a specific current value.
+    /// </summary>
+    /// <param name="dieValue">
+    /// An integer value of the number of the dice to lock.
+    /// </param>
+    private void LockDice(int dieValue) {
+      this._dice
+        .Where(die => die.Value == dieValue)
+        .ToList<Die>()
+        .ForEach(die => die.Locked = true);
+    }
+
+    public static int TwoOfAKind(int[] diceRolled) {
+      foreach (KeyValuePair<int, int> kv in ThreeOrMore.GetFrequencies(diceRolled)) {
         if (kv.Value != 2) {
           continue;
         }
@@ -67,20 +106,6 @@ namespace CMP1903_A2_2324 {
         return kv.Key;
       }
       return -1;
-    }
-
-    private bool LockTwoDice(int dieValue) {
-      Die[] foundDice = this._dice
-        .Where(die => die.Value == dieValue)
-        .ToArray();
-
-      if (foundDice.Length < 2) {
-        return false;
-      }
-
-      foundDice[0].Locked = true;
-      foundDice[1].Locked = true;
-      return true;
     }
 
     public static int GetScoreFromValues(int[] values) {
